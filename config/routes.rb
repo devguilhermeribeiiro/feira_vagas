593f7f0caa80f9cb82556170053e836936d22c3a
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-20 15:36:23 -0300

    Adding de start route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :job_seekers
   devise_for :employers
 
+  get 'start', to: 'home#index', as: :start
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
