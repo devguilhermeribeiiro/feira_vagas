47005386cd39fcb512eec14f0aaca36327e25272
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-24 15:50:00 -0300

    Removing the image field from job_seekers

---
+++ db/migrate/20240824184811_remove_profile_img_from_job_seekers.rb
@@ -0,0 +1,5 @@
+class RemoveProfileImgFromJobSeekers < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :job_seekers, :profile_img, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_24_184451) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_24_184811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,7 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_24_184451) do
     t.string "degree"
     t.text "courses"
     t.text "experiences"
-    t.string "profile_img"
     t.index ["email"], name: "index_job_seekers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
   end
