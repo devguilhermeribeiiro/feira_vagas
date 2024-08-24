47a0e1a75c404c4c70334d2219b04f77c6181446
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-24 15:15:14 -0300

    Creating the job_seeker controller

---
+++ app/controllers/job_seekers_controller.rb
@@ -0,0 +1,30 @@
+class JobSeekersController < ApplicationController
+  before_action :set_job_seeker, only: %i[ show edit update destroy ]
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @job_seeker.update(job_seeker_params)
+      redirect_to job_seeker_url(@job_seeker), notice: 'Perfil atualizado com sucesso'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @job_seeker.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def set_job_seeker
+    @job_seeker = JobSeeker.find(current_job_seeker.id)
+  end
+
+  def job_seeker_params
+    params.require(:job_seeker).permit(%i[ full_name phone email degree courses experiences])
+  end
+end
