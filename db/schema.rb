102ed2f2157c3ea8b12c05db154972b28c368086
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-23 21:50:13 -0300

    Adding the phone field to job seekers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_141620) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_24_002736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_141620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
+    t.string "phone"
     t.index ["email"], name: "index_job_seekers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
   end
