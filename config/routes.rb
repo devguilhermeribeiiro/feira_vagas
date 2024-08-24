2677514771020475efd91471bb179dffe454e401
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-24 15:19:52 -0300

    Setting the routes to job_seekers_controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
   root "home#welcome"
 
   resources :jobs
   resources :home
+  resources :job_seekers, only: %i[ edit update destroy show ]
 
   devise_for :job_seekers
   devise_for :employers
+
   devise_scope :job_seeker do
     get '/job_seekers/sign_out' => 'devise/sessions#destroy'
   end
@@ -14,7 +17,6 @@ Rails.application.routes.draw do
   end
 
   get 'start', to: 'home#index', as: :start
-  get 'profile', to: 'home#profile', as: :job_seeker_profile
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
