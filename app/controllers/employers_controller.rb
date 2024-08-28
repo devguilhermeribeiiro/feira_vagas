6acdb20be4cfe6d08376117dce85f570f332b414
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-27 22:07:30 -0300

    setting the controller methods

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,7 +1,32 @@
 class EmployersController < ApplicationController
+  before_action :set_employer, only: %i[ show edit update destroy ]
+  before_action :authenticate_employer!
+
   def show
   end
 
   def edit
+
+  def update
+    if @employer.update(employer_params)
+      redirect_to employer_url(@employer), notice: 'Perfil atualizado com sucesso'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @employer.destroy
+    redirect_to root_path
+  end
+  
+  private
+
+  def set_employer
+    @employer = Employer.find(params[:id])
+  end
+  
+  def employer_params 
+    params.require(@employer).permit(%i[ company_name email location ])
   end
 end
