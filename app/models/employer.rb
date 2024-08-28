b05df18be6f7dca64e422c97460d9103ddee17e8
guilherme <devguilhermeribeiro000@gmail.com>
2024-08-28 13:49:40 -0300

    Adding the image field on employer model

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -6,5 +6,17 @@ class Employer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :correct_profile_img_mime_type
+  has_one_attached :profile_img
   has_many :jobs
+  
+  private
+
+  def correct_profile_img_mime_type
+    if profile_img.attached? && !profile_img.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:profile_img, 'deve ser uma imagem JPG, PNG ou GIF')
+    elsif profile_img.attached? && profile_img.byte_size > 5.megabytes
+      errors.add(:profile_img, 'deve ser menor que 5MB')
+    end
+  end
 end
