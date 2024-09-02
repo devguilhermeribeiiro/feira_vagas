e6d737f7f9fb42ec9db1feadb06873c3a88f3f13
guilherme <devguilhermeribeiro000@gmail.com>
2024-09-02 16:48:30 -0300

    Adding comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'feiravagas.onrender.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com'
+    address:              'smtp.gmail.com',
     port:                 587,
     domain:               'feiravagas.onrender.com',
     user_name:            ENV['EMAIL_USER_NAME'],
