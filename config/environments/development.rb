8c53dde4e39fdbce3a79161ba55a8bdaf2b35d7f
guilherme <devguilhermeribeiro000@gmail.com>
2024-09-02 23:08:20 -0300

    Configure mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,9 +74,12 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
